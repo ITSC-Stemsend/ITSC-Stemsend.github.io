f1329cd4f6a3f4ab1e6fe7efb23be682ce312558
ITSC-Stemsend <mikunakano305@gmail.com>
2023-07-14 07:46:54 +0700

    Menambah Fitur Form

--- js/ITSC.js
+++ js/ITSC.js
@@ -14,3 +14,16 @@ document.addEventListener('click', function(e){
   if(!hamburger.contains(e.target) && !navbarNav.contains(e.target))
     navbarNav.classList.remove('active');
 });
+
+function showLoading() {
+  var btn = document.getElementById("loadingBtn");
+  var loader = document.createElement("div");
+  loader.className = "loader";
+  btn.innerHTML = ""; // Menghapus teks pada tombol
+  btn.appendChild(loader);
+  loader.style.display = "inline-block";
+  setTimeout(function() {
+    loader.style.display = "none";
+    btn.innerHTML = "Kirim Pesan";
+  }, 3000); // Menunggu 3 detik sebelum mengembalikan tombol ke kondisi semula
+}
