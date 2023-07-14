d3ef558041beaef82446a911bb6428cf2759c8ba
ITSC-Stemsend <mikunakano305@gmail.com>
2023-07-14 08:26:38 +0700

    memperbaiki form

--- js/ITSC.js
+++ js/ITSC.js
@@ -14,22 +14,3 @@ document.addEventListener('click', function(e){
   if(!hamburger.contains(e.target) && !navbarNav.contains(e.target))
     navbarNav.classList.remove('active');
 });
-
-function showLoading() {
-  var btn = document.getElementById("loadingBtn");
-  var loader = document.createElement("div");
-  loader.className = "loader";
-  btn.innerHTML = ""; // Menghapus teks pada tombol
-  btn.appendChild(loader);
-  loader.style.display = "inline-block";
-  setTimeout(function() {
-    loader.style.display = "none";
-    btn.innerHTML = "Kirim";
-  }, 3000); // Menunggu 3 detik sebelum mengembalikan tombol ke kondisi semula
-
-   // Setelah pengiriman selesai, lakukan pembaruan halaman setelah penundaan tertentu
-   setTimeout(function() {
-    location.reload();
-  }, 2000); // Mengatur waktu penundaan dalam milidetik sebelum memperbarui halaman (2 detik dalam contoh ini)
-}
-
