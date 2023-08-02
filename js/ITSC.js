f19d54b0574157afd49c5bbf92a904ddce928bf7
ITSC-Stemsend <mikunakano305@gmail.com>
2023-08-03 05:29:51 +0700

    membuat Navbar Scroll

--- js/ITSC.js
+++ js/ITSC.js
@@ -14,3 +14,15 @@ document.addEventListener('click', function(e){
   if(!hamburger.contains(e.target) && !navbarNav.contains(e.target))
     navbarNav.classList.remove('active');
 });
+
+
+// script.js
+window.onscroll = function() {
+    const nav = document.querySelector('.navbar');
+    if(window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+};
+
